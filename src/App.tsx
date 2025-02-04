5f2395f2f27a6519c6f5528931a16c32be10a2b3
Jenna-Deamer <200529678@student.georgianc.on.ca>
2025-02-04 08:28:03 -0500

    Add ProductSidebar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,36 +1,32 @@
 /** DOCS for react-three-fiber an easier way to use Three.js in react apps *
  * https://r3f.docs.pmnd.rs/getting-started/introduction
  * **/
-import { Canvas } from '@react-three/fiber'; // Auto setups a three.js scene, camera, and renderer
-import { OrbitControls, useGLTF } from '@react-three/drei'; // Allow users to rotate, zoom, and pan the 
+import { Canvas } from "@react-three/fiber"; // Auto setups a three.js scene, camera, and renderer
+import { OrbitControls, useGLTF } from "@react-three/drei"; // Allow users to rotate, zoom, and pan the
+import ProductSidebar from "./components/ProuductSidebar";
 
 function Model() {
-    const counter = useGLTF('./src/assets/counters/straightCounter.glb')
+    const counter = useGLTF("./src/assets/counters/straightCounter.glb");
     return (
-        <primitive
-            object={counter.scene}
-            scale={0.1}
-            position={[1, 0, 0]}
-        />
-    )
+        <primitive object={counter.scene} scale={0.1} position={[1, 0, 0]} />
+    );
 }
 
 function App() {
     return (
         <main>
+            <ProductSidebar />
             <Canvas
                 camera={{
                     position: [0, 2.5, 10], // x, y, zs
-
                 }}
             >
                 <ambientLight intensity={1} /> {/* Soft, even light */}
                 <Model />
                 <OrbitControls />
             </Canvas>
-
         </main>
-    )
+    );
 }
 
-export default App
+export default App;
---
+++ src/components/ProuductSidebar.tsx
@@ -0,0 +1,9 @@
+function ProductSidebar() {
+    return (
+        <aside>
+            <h2>Product Sidebar</h2>
+        </aside>
+    );
+}
+
+export default ProductSidebar;

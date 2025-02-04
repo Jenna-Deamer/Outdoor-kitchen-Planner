19efb5bfe402286ee104daebd8299108aba03b5b
Jenna Deamer <jennadeamer@gmail.com>
2025-02-03 21:53:45 -0500

    Add 3D model of straight counter and update scene configuration

--- src/App.tsx
+++ src/App.tsx
@@ -2,25 +2,35 @@
  * https://r3f.docs.pmnd.rs/getting-started/introduction
  * **/
 import { Canvas } from '@react-three/fiber'; // Auto setups a three.js scene, camera, and renderer
-import { OrbitControls } from '@react-three/drei'; // Allow users to rotate, zoom, and pan the 
+import { OrbitControls, useGLTF } from '@react-three/drei'; // Allow users to rotate, zoom, and pan the 
+
+function Model() {
+    const counter = useGLTF('./src/assets/counters/straightCounter.glb')
+    return (
+        <primitive
+            object={counter.scene}
+            scale={0.1}
+            position={[1, 0, 0]}
+        />
+    )
+}
 
 function App() {
-  return (
-   <main>
-    <h1>Cool Cube</h1>
-<div className='canvas-container'>
-    <Canvas>
-    <ambientLight intensity={0.5} /> {/* Soft, even light */}
-    <mesh>
-    <boxGeometry args={[1, 1, 1]} /> {/* Width, height, depth */}
-    <meshStandardMaterial color="orange" />
-  </mesh>
-  <OrbitControls /> 
-    </Canvas>
-    </div>
-   </main>
-  )
+    return (
+        <main>
+            <Canvas
+                camera={{
+                    position: [0, 2.5, 10], // x, y, zs
+
+                }}
+            >
+                <ambientLight intensity={1} /> {/* Soft, even light */}
+                <Model />
+                <OrbitControls />
+            </Canvas>
+
+        </main>
+    )
 }
 
 export default App
-

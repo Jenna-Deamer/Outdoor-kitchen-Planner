76c649dbc1f0b926743e7c7ce78105a5de547538
Jenna Deamer <jennadeamer@gmail.com>
2025-03-25 15:14:32 -0400

    Move Modal outside of Sidebar for better pos  on mobile

--- src/components/ModelSidebar.tsx
+++ src/components/ModelSidebar.tsx
@@ -42,66 +42,69 @@ function ModelSidebar({
             onClick={toggleSidebar}
             aria-label="Toggle sidebar"
             aria-expanded={isSidebarOpen}
-        />
-            <aside className={isSidebarOpen ? 'active' : ''}>
-                <div id="sidebar-header">
-                    <h2>Design Tools</h2>
-                    <button id="add-appliance-btn" onClick={handleOpenModal}>
-                        Add Appliance
-                    </button>
-                </div>
-                <div id="sidebar-model-list">
-                    {models.map((model, index) => (
-                        <div key={index} className="model-control-card">
-                            <h3>
-                                {model.type.charAt(0).toUpperCase() +
-                                    model.type.slice(1)}
-                            </h3>
-                            <div className="model-control-buttons">
-                            <button onClick={() => onSelectModel(index)}>Select</button>
-                                <button onClick={() => onDeleteModel(index) }>Delete</button>
-                            </div>
-                        </div>
-                    ))}
-                </div>
-                <div id="sidebar-footer">
-                    <small>
-                        Click on a model to select, then use the arrow keys
-                        to&nbsp;move!
-                    </small>
-                </div>
-                <AddApplianceModal isOpen={isModalOpen} onClose={handleCloseModal}>
-                    <h2>Select an Appliance</h2>
-                    {modelData.map((item) => (
-                        <div key={item.id} className="appliance-selection-card">
-                            <img src={item.imagePath} alt={item.name} />
-                            <div className="appliance-details">
-                                <p>{item.name}</p>
-                                <small>
-                                    {item.dimensions.width}W x{" "}
-                                    {item.dimensions.height}H x{" "}
-                                    {item.dimensions.depth}D
-                                </small>
-                                <small>{item.description}</small>
-                            </div>
-                            <div className="modal-button-container">
-                                <button
-                                    onClick={() => {
-                                        if (item.type === "cabinet") {
-                                            onAddCabinet();
-                                        } else {
-                                            onAddFridge();
-                                        }
-                                        handleCloseModal();
-                                    }}
-                                >
-                                    <PlusIcon />
-                                </button>
-                            </div>
-                        </div>
-                    ))}
-                </AddApplianceModal>
-            </aside>
+          />
+          
+          <aside className={isSidebarOpen ? 'active' : ''}>
+              <div id="sidebar-header">
+                  <h2>Design Tools</h2>
+                  <button id="add-appliance-btn" onClick={handleOpenModal}>
+                      Add Appliance
+                  </button>
+              </div>
+              <div id="sidebar-model-list">
+                  {models.map((model, index) => (
+                      <div key={index} className="model-control-card">
+                          <h3>
+                              {model.type.charAt(0).toUpperCase() +
+                                  model.type.slice(1)}
+                          </h3>
+                          <div className="model-control-buttons">
+                          <button onClick={() => onSelectModel(index)}>Select</button>
+                              <button onClick={() => onDeleteModel(index) }>Delete</button>
+                          </div>
+                      </div>
+                  ))}
+              </div>
+              <div id="sidebar-footer">
+                  <small>
+                      Click on a model to select, then use the arrow keys
+                      to&nbsp;move!
+                  </small>
+              </div>
+          </aside>
+          
+      
+          <AddApplianceModal isOpen={isModalOpen} onClose={handleCloseModal}>
+              <h2>Select an Appliance</h2>
+              {modelData.map((item) => (
+                  <div key={item.id} className="appliance-selection-card">
+                      <img src={item.imagePath} alt={item.name} />
+                      <div className="appliance-details">
+                          <p>{item.name}</p>
+                          <small>
+                              {item.dimensions.width}W x{" "}
+                              {item.dimensions.height}H x{" "}
+                              {item.dimensions.depth}D
+                          </small>
+                          <small>{item.description}</small>
+                      </div>
+                      <div className="modal-button-container">
+                          <button
+                              onClick={() => {
+                                  if (item.type === "cabinet") {
+                                      onAddCabinet();
+                                  } else {
+                                      onAddFridge();
+                                  }
+                                  handleCloseModal();
+                              }}
+                          >
+                              <PlusIcon />
+                          </button>
+                      </div>
+                  </div>
+              ))}
+          </AddApplianceModal>
         </>
     );
 }
